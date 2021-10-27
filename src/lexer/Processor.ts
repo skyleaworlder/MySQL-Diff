4435f10081b11664e44b8406a38ae4adc037527a
skyleaworlder <870033938@qq.com>
2021-10-28 00:58:05 +0800

    add: lexer about constraint

--- src/lexer/Processor.ts
+++ src/lexer/Processor.ts
@@ -1,11 +1,12 @@
-import { BaseConstraint, CheckConstraint, ForeignKeyConstraint } from "@/model/Constraint";
+import { BaseConstraint, CheckConstraint, FkConstraintOnStatement, ForeignKeyConstraint } from "@/model/Constraint";
 import { DataColumn, DataColumnOptions } from "@/model/DataColumn";
 import { NormalKey, PrimaryKey, UniqueKey, KeyOptions, BaseKey } from "@/model/Key";
 import { Table } from "@/model/Table";
-import { IndexDS, KeyType } from "@/model/Enum";
+import { ConstraintType, IndexDS, KeyType } from "@/model/Enum";
 
 import { fetchTableName } from "@/util/Table";
 import { fetchKeyType, fetchKeyName, fetchKeyPart, unmarshalKeyOptions } from "@/util/Key";
+import { fetchConstraintConditions, fetchConstraintName, fetchConstraintType, fetchFkConstraintCols, fetchFkConstraintOn, fetchFkConstraintRefTblCols, fetchFkConstraintRefTblName } from "@/util/Constraint";
 
 export function processTable(table_ddl: Array<String>): Table {
   let table_name: String = "";
@@ -30,7 +31,14 @@ export function processTable(table_ddl: Array<String>): Table {
       continue;
     }
 
-    // TODO: about constraint
+    // about constraint
+    const constraint_type = fetchConstraintType(line);
+    if (constraint_type != null) {
+      constraints.push(processConstraint(line, constraint_type));
+      continue;
+    }
+
+    // TODO: data column
   }
   return new Table(table_name, [], [], []);
 }
@@ -51,5 +59,26 @@ function processKey(ddl: String, key_type: KeyType | null): BaseKey {
     default:
       console.error("processKey error: unknown key type!");
       return new BaseKey(KeyType.NORMAL_KEY, [], undefined, new KeyOptions());
+  }
+}
+
+function processConstraint(ddl: String, constraint_type: ConstraintType): BaseConstraint {
+  let constraint_name: String = fetchConstraintName(ddl);
+  switch (constraint_type) {
+    case ConstraintType.CHECK_CONSTRAINT:
+      let constraint_conditions: String = fetchConstraintConditions(ddl);
+      return new CheckConstraint(constraint_name, constraint_conditions);
+    case ConstraintType.FOREIGN_KEY_CONSTRAINT:
+      let fk_constraint_cols: Array<String> = fetchFkConstraintCols(ddl);
+      let fk_constraint_refer_tbl: String = fetchFkConstraintRefTblName(ddl);
+      let fk_constraint_refer_cols: Array<String> = fetchFkConstraintRefTblCols(ddl);
+      let fk_constraint_on: Array<FkConstraintOnStatement> = fetchFkConstraintOn(ddl);
+      return new ForeignKeyConstraint(
+        constraint_name, fk_constraint_cols, fk_constraint_refer_tbl,
+        fk_constraint_refer_cols, fk_constraint_on
+      );
+    default:
+      console.error("processConstraint error: unknown constraint type!");
+      return new BaseConstraint("", ConstraintType.CHECK_CONSTRAINT);
   }
 }
