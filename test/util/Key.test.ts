4b55de835279a79fa479aefba31f458b0ced00de
skyleaworlder <870033938@qq.com>
2021-10-28 13:50:30 +0800

    add: test/util/Key.test.ts

--- test/util/Key.test.ts
+++ test/util/Key.test.ts
@@ -1,14 +1,33 @@
-import { fetchKeyPart } from "@/util/Key";
+import { IndexDS, KeyType } from "@/model/Enum";
+import { KeyOptions } from "@/model/Key";
+import { fetchKeyName, fetchKeyPart, fetchKeyType, unmarshalKeyOptions } from "@/util/Key";
 
-test('get key part from ddl', () => {
-  let key_part: Array<String>;
-  // UNIQUE KEY
-  key_part = fetchKeyPart("  UNIQUE KEY `name` (`name`, `c_id`),");
-  expect(key_part).toEqual(["name", "c_id"]);
-  // KEY
-  key_part = fetchKeyPart("KEY `c_id` (`c_id`),");
-  expect(key_part).toEqual(["c_id"]);
-  // PRIMARY KEY
-  key_part = fetchKeyPart("  PRIMARY KEY (`id`),");
-  expect(key_part).toEqual(["id"]);
+test('fetch key', () => {
+  let test_case_npk = [
+    {input: "  UNIQUE KEY `name` (`name`, `c_id`),", expect: {
+      name: "name", type: KeyType.UNIQUE_KEY, part: ["name", "c_id"], ds: IndexDS.BTREE, ops: new KeyOptions()
+    }},
+    {input: "KEY `c_id` (`c_id`),", expect: {
+      name: "c_id", type: KeyType.NORMAL_KEY, part: ["c_id"], ds: IndexDS.BTREE, ops: new KeyOptions()
+    }}
+  ]
+  test_case_npk.forEach(t_case => {
+    const type = fetchKeyType(t_case.input);
+    expect(type).toBe(t_case.expect.type);
+    expect(fetchKeyName(t_case.input, type!)).toBe(t_case.expect.name);
+    expect(fetchKeyPart(t_case.input)).toEqual(t_case.expect.part);
+    expect(unmarshalKeyOptions(t_case.input)).toEqual(t_case.expect.ops);
+  })
+
+  let test_case_pk = [
+    {input: "  PRIMARY KEY (`id`),", expect: {
+      type: KeyType.PRIMARY_KEY, part: ["id"], ds: IndexDS.BTREE, ops: new KeyOptions()
+    }}
+  ]
+  test_case_pk.forEach(t_case => {
+    const type = fetchKeyType(t_case.input);
+    expect(type).toBe(t_case.expect.type);
+    expect(fetchKeyPart(t_case.input)).toEqual(t_case.expect.part);
+    expect(unmarshalKeyOptions(t_case.input)).toEqual(t_case.expect.ops);
+  })
 })
