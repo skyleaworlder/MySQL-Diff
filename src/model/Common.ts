ba6e755dadeb1e317c600a5c22c819c730967cae
skyleaworlder <870033938@qq.com>
2021-10-31 16:05:02 +0800

    add: Common interface definition

---
+++ src/model/Common.ts
@@ -0,0 +1,38 @@
+import { Difference } from "@/model/Difference";
+
+/**
+ * Appender<T>
+ * 所有的 container class 都需要实现的 interface。
+ * 目的是让 class 中的 Map 如列表般操作。
+ */
+export interface Appender<T> {
+  append(elem: T): void;
+}
+
+
+/**
+ * Comparer<T>
+ * 所有 data model 都要实现的接口。因为所有 model 都是可以比较的。
+ * table / column / key / constraint 都需要支持。
+ */
+export interface Comparer<T> {
+  compareTo(that: T): Array<Difference<any>>;
+}
+
+
+/**
+ * Equaler<T>
+ * 所有基本单元都要实现这个 interface 供 container class 调用。
+ */
+export interface Equaler<T> {
+  equal(that: T): boolean;
+}
+
+
+/**
+ * Transformer<T>
+ * 最后 DDL 语句的输出。
+ */
+export interface Transformer<T> {
+  transform(difference: Difference<T>): Array<String>;
+}
