8ce6cb842091f9235bbf1ca22388af18a9581563
skyleaworlder <870033938@qq.com>
2021-11-01 22:00:10 +0800

    fix: test

--- src/model/DDL.ts
+++ src/model/DDL.ts
@@ -1,9 +1,10 @@
 import { Table } from "@/model/Table";
 
 export class DDL {
-    tables: Array<Table>
+  tables: Array<Table>
 
-    constructor(tables: Array<Table>) {
-        this.tables = tables;
-    }
+  constructor(tables: Array<Table>) {
+      this.tables = tables;
+  }
+
 }
