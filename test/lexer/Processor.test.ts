a0735b9381d9258a7c7e7db69137d3d033c06e5c
skyleaworlder <870033938@qq.com>
2021-11-01 22:00:35 +0800

    test: table comparer, transformer

--- test/lexer/Processor.test.ts
+++ test/lexer/Processor.test.ts
@@ -79,3 +79,46 @@ test('process a ddl file', () => {
   let ddl = processDDL(input);
   console.log(ddl);
 })
+
+
+test('compare & transform 2 tables', () => {
+  let input_1 = [
+    'CREATE TABLE `test_constraint` (',
+    '  `id` int NOT NULL AUTO_INCREMENT,',
+    '  `c1` int DEFAULT NULL,',
+    '  `c2` int DEFAULT NULL,',
+    '  `c3` int DEFAULT NULL,',
+    "  `c_id` int NOT NULL COMMENT 'c_id',",
+    "  `d_id` int NOT NULL,",
+    '  PRIMARY KEY (`id`),',
+    '  CONSTRAINT `test_constraint_chk_2` CHECK ((`c3` < 100)),',
+    '  CONSTRAINT `test_constraint_chk_3` CHECK ((`c1` <> `c2`)),',
+    '  CONSTRAINT `test_constraint_chk_4` CHECK ((`c1` > `c3`))',
+    '  CONSTRAINT `test_index_ibfk_1` FOREIGN KEY (`c_id`, `d_id`) REFERENCES `test_datacolumn` (`c_id`, `id`) ON DELETE CASCADE',
+    ') ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;'
+  ];
+  let input_2 = [
+    'CREATE TABLE `test_constraint` (',
+    '  `id` int NOT NULL AUTO_INCREMENT,',
+    '  `c1` int DEFAULT NULL,',
+    '  `c2` int DEFAULT NULL,',
+    '  `c3` int DEFAULT NULL,',
+    "  `c_id` int NOT NULL COMMENT 'c_id',",
+    "  `d_id` int NOT NULL,",
+    '  PRIMARY KEY (`id`),',
+    '  CONSTRAINT `c1_nonzero` CHECK ((`c1` <> 0)),',
+    '  CONSTRAINT `test_constraint_chk_2` CHECK ((`c3` < 100)),',
+    '  CONSTRAINT `test_constraint_chk_3` CHECK ((`c1` <> `id`)),',
+    '  CONSTRAINT `test_constraint_chk_4` CHECK ((`c1` > `c3`))',
+    '  CONSTRAINT `test_index_ibfk_1` FOREIGN KEY (`c_id`) REFERENCES `test_datacolumn` (`c_id`) ON DELETE CASCADE,',
+    '  CONSTRAINT `test_index_ibfk_2` FOREIGN KEY (`d_id`) REFERENCES `test_datacolumn` (`id`) ON DELETE CASCADE',
+    ') ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;'
+  ];
+
+  let [tbl_a] = processTable(input_1);
+  let [tbl_b] = processTable(input_2);
+  let diff = tbl_a.compareTo(tbl_b);
+  let ddl = tbl_a.transform(diff);
+  console.log(diff);
+  console.log(ddl);
+})
